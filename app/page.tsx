9a7cbcdc86b402a4b91ebe7b744d2bfa2c86c30d
samueltexa <woolardsamuels@gmail.com>
2024-07-23 11:32:57 +0300

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
