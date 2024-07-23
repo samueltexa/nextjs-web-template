f74a9441d14e900eaff95e6c800be3610d3ba89d
samueltexa <woolardsamuels@gmail.com>
2024-07-23 13:56:38 +0300

    add fonts

---
+++ app/src/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
